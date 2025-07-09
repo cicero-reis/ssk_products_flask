55b8c708af0fa0c487ac0763d07de9c2927daf43
ciceroreis <ciceroreis@outlook.com>
2025-07-09 11:47:35 -0300

    DEV-016 - Add Swagger UI

--- setup_tests.sh
+++ setup_tests.sh
@@ -3,6 +3,6 @@
 
 echo "Instalando dependências de teste..."
 pip install -r docker/requirements.txt
-pip install -r requirements-test.txt
+#pip install -r requirements-test.txt
 
 echo "Dependências instaladas com sucesso!"
